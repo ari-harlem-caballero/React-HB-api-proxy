4ec0f2cde78a4af1c61b7fe071638530e7f81d77
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-15 08:47:15 -0800

    made pokemonlist

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,6 +1,15 @@
-<div>
-        {pokename.map((pokemon, i) => <div className='pokemon-info' key={pokemon.pokemon + i}>
-          <p>{pokemon.pokemon}</p>
-          <img src={pokemon.url_image} />
-        </div>)}
-      </div>
+import React from 'react';
+
+export default function PokemonList({ pokemonName }) {
+  return (
+    <div>
+      {
+        pokemonName.map((pokemon, i) => 
+          <div className='pokemon-info' key={pokemon.pokemon + i}>
+            <p>{pokemon.pokemon}</p>
+            <img src={pokemon.url_image} />
+          </div>)
+      }
+    </div>
+  );
+}
