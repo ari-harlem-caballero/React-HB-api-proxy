339e6d3037c96de810fbcbd4f9acb34135580561
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-15 08:50:43 -0800

    added spinner.js

--- src/Spinner.js
+++ src/Spinner.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Spinner() {
   return (
-    <div>Spinner</div>
+    <div>
+      <img src='./pokemon_spinner.gif' alt='Charmander pokemon spinning in a circle to chase its fire tail'/>
+    </div>
   );
 }
