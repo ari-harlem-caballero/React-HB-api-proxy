6ea2d49b13b0618b6c2be9ebe6bac5aee0d68548
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-16 09:38:32 -0800

    added weather data api fetch

--- functions/weather.js
+++ functions/weather.js
@@ -7,22 +7,27 @@ exports.handler = async (e) => {
     // grab the city, state, and country from the request's query parameters
     // here is an example from the netlify docs:
     // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D 
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Portland,OR,US&limit=1&appid=322c3fadb25f52adb10594adb37f7518`);
+    const geoCodeResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Portland,OR,US&limit=1&appid=${process.env.WEATHER_KEY}`);
 
     // tragicly, we cannot just pass the city name to this API. it wants a latitude and longitude for the weather
 
-    const [{ lat, lon }] = await response.json();
+    const geoCodeJson = await geoCodeResponse.json();
+
+    const latitude = geoCodeJson[0].lat;
+    const longitude = geoCodeJson[0].lon;
     // consult the yelp docs to figure out how to use a city, state, and country to make a request and get the latitude and longitude
     // https://openweathermap.org/api/geocoding-api
 
+    const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=45.5202471&lon=-122.6741949&appid=${process.env.WEATHER_KEY}&units=imperial`);
     // once you have gotten the lat/lon using the geocoding api, use the lat/lon to get the weather. Consult the docs below:
     // https://openweathermap.org/api/one-call-api
+    const weatherJson = await weatherResponse.json();
 
 
     return { 
       statusCode: 200, 
     // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the weather data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
-      body: JSON.stringify(lat, lon),
+      body: JSON.stringify(weatherJson),
     };
   } catch (error) {
     console.log(error);
