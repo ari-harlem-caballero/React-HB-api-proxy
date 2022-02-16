d7fe14cc938e12642657c4a1eac2c287e5d8d5f2
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-16 09:26:37 -0800

    starting weather component, added spinner.js

---
+++ src/WeatherSpinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function WeatherSpinner() {
+  return (
+    <div>
+      <img src='./weather_spinner.gif' alt='loading gif of wind transitioning to sun to clouds to moon'/>
+    </div>
+  );
+}
