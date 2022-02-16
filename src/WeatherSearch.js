c17b43f71a884b9419c39fdcc7bafe5ed3320171
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-16 09:50:48 -0800

    weatherSearch set state and created submitHandler

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -2,15 +2,27 @@ import { useState } from 'react';
 
 export default function WeatherSearch() {
       // you'll need to track your weather search results, the loading state, and a form field for location with a default value.
+  const [weatherSearch, setWeatherSearch] = useState([]);
+  const [city, setCity] = useState('astoria');
+  const [state, setState] = useState('or');
+  const [country, setCountry] = useState('usa');
+  const [loading, setLoading] = useState(false);
+
   
   async function handleWeatherSubmit(e) {
     e.preventDefault();
       
         // set the loading state to true
+    setLoading(true);
         // use fetch to make a request to your netlify weather function. Be sure to pass the location as a query param in the URL
-      
+    const response = await fetch(`/.netlify/functions/weather?city=${city}&state=${state}&country=${country}`);
   
         // put the jsonified data in state and set the loading state to false
+    const json = await response.json();
+
+    setWeatherSearch(json);
+
+    setLoading(false);
   }
       
   return (
